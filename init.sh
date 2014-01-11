ca4166dc947f38316b0c17c3f0681ea3596f59ed
Tom Molin <tom.molin@gmail.com>
2014-01-11 18:45:32 +0100

    Added sysctl -w kernel.shmmax=714219520

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@
 /postgres.sh &
 
 # Start Atlassian in the forground
+sysctl -w kernel.shmmax=714219520
 su - "$STASHUSR" -c "export STASH_HOME=$STASH_HOME ; /opt/atlassian/atlassian-stash-$AppVer/bin/start-stash.sh -fg"
